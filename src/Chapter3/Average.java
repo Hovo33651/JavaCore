7ef90ae1b1c531d195e0c999ca516ed48fcdcf16
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-02 22:15:34 +0400

    Chapter 3/2 Homework

--- src/Chapter3/Average.java
+++ src/Chapter3/Average.java
@@ -3,7 +3,7 @@ package Chapter3;
 public class Average {
     public static void main(String[] args) {
         int nums[] = {10, 20, 30, 40, 50};
-        int result = 0;
+        int result =0;
         for (int i = 0; i < 5; i++) {
             result = result + nums[i];
             System.out.println("result is " + result / 5);
