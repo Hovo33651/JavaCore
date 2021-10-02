73e9d97f8e491667573dfa6d18283f7184f7b326
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-02 19:25:59 +0400

    Chapter 3/2 Homework

---
+++ src/Chapter3/DynInit.java
@@ -0,0 +1,11 @@
+package Chapter3;
+
+public class DynInit {
+    public static void main(String[] args) {
+        double a,b,c;
+        a = 3.0;
+        b=4.0;
+        c= Math.sqrt(a*a+b*b);
+        System.out.println("nerqnadzig = " + c);
+    }
+}
