77c52ebc9194bc803de285553334613e5953959e
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-02 13:54:31 +0400

    add Example2 to Chapter1

--- src/Chapter1/Example2.java
+++ src/Chapter1/Example2.java
@@ -4,6 +4,8 @@ public class Example2 {
     public static void main(String[] args) {
         int num;
         num = 100;
+        System.out.println("int num " + num);
+        num = num * 2;
         System.out.println("int num" + num);
     }
 
