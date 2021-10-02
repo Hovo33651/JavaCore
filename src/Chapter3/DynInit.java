19b482198aff40bcae953c99ff7fa3768a2610c1
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-02 19:35:30 +0400

    Chapter 3/2 Homework

--- src/Chapter3/DynInit.java
+++ src/Chapter3/DynInit.java
@@ -2,10 +2,22 @@ package Chapter3;
 
 public class DynInit {
     public static void main(String[] args) {
-        double a,b,c;
-        a = 3.0;
-        b=4.0;
-        c= Math.sqrt(a*a+b*b);
-        System.out.println("nerqnadzig = " + c);
+        {
+            double a, b, c;
+            a = 3.0;
+            b = 4.0;
+            c = Math.sqrt(a * a + b * b);
+            System.out.println("nerqnadzig = " + c);
+        }
+        double Daxod, Rojik, Hark, Mnacord;
+        Daxod = 1_500_000;
+        Hark = Daxod * 24 / 100;
+        Rojik = (Daxod - Hark) * 15 / 100;
+        Mnacord = Daxod - Hark - Rojik;
+        System.out.println(Mnacord);
+
+
     }
+
 }
+
