d8f1ac207094cbf35895acc87d60936779dfc3d1
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-02 17:16:08 +0400

    Chapter 3/1 Homework

--- src/Chapter3/BoolTest.java
+++ src/Chapter3/BoolTest.java
@@ -8,10 +8,10 @@ public class BoolTest {
         b = true;
         System.out.println("b is " + b);
 
-        if(b) System.out.println("b was " + b);
+        if (b) System.out.println("b was " + b);
         b = false;
-        if(b) System.out.println("b was not " + b);
+        if (b) System.out.println("b was not " + b);
 
-        System.out.println("10 > 9 is " + (10 > 9 ));
+        System.out.println("10 > 9 is " + (10 > 9));
     }
 }
--- src/Chapter3/CharDemo.java
+++ src/Chapter3/CharDemo.java
@@ -5,15 +5,15 @@ public class CharDemo {
         char c1;
         char c2;
         char c3;
-        c1='J';
-        c2='A';
-        c3='V';
-        System.out.println(c1+c2+c3+c2);
-        System.out.println(c1+""+c2+""+c3+""+c2);
+        c1 = 'J';
+        c2 = 'A';
+        c3 = 'V';
+        System.out.println(c1 + c2 + c3 + c2);
+        System.out.println(c1 + "" + c2 + "" + c3 + "" + c2);
 
-        char ch1,ch2;
-        ch1=88;
-        ch2='Y';
+        char ch1, ch2;
+        ch1 = 88;
+        ch2 = 'Y';
         System.out.print("ch1 and ch2: ");
         System.out.println(ch1 + " " + ch2);
 
