7d617bf9106757d73c569abe012f0a14601e8136
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-03 17:41:38 +0400

    Chapter 3/2 Homework

--- src/Chapter3/Area.java
+++ src/Chapter3/Area.java
@@ -7,5 +7,19 @@ public class Area {
         pi = 3.1416;
         a = pi * r * r;
         System.out.println("the area of circle is " + a);
+
+
+        int q = 20;
+        int b = 25;
+        double c = Math.sqrt((q * q) + (b * b));
+        int angle_qb = 90;
+        int angle_bc = 43;
+        int angle_qc = 47;
+        double sin_angle_bc = q / c;
+        double cos_angle_bc = b / c;
+        System.out.println(sin_angle_bc + "," + cos_angle_bc);
+
+
     }
+
 }
