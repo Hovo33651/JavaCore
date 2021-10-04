62abd49b5888368a660dea83e0ed6cc3f0dbc27d
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-04 21:40:25 +0400

    Homework1

--- src/Chapter3/AutoArray.java
+++ src/Chapter3/AutoArray.java
@@ -5,8 +5,9 @@ public class AutoArray {
         int[] monthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,
                 31};
         System.out.println("April has " + monthDays[3] + " days");
-
-
+        for (int i = 0; i < 10; i++) {
+            System.out.print(monthDays[i] + " ");
+        }
     }
 
 
