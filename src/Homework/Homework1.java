dd231be109b0100ef2ea10e90a3eb17afd257c79
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-04 22:26:32 +0400

    Homework1

--- src/Homework/Homework1.java
+++ src/Homework/Homework1.java
@@ -3,7 +3,7 @@ package Homework;
 public class Homework1 {
     public static void main(String[] args) {
 
-        for (int i = 1; i < 15; i++) {
+        for (int i = 1; i < 6; i++) {
             for (int j = 0; j < i - 1; j++) {
                 System.out.print('*');
             }
