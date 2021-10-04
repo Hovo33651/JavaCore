73001c863f33cec917c1a58bd7d01e1720240ec6
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-05 01:36:29 +0400

    Homework1

---
+++ src/Homework/Homework1Backwords.java
@@ -0,0 +1,15 @@
+package Homework;
+
+public class Homework1Backwords {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = i-2; j < 3  ; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+
+
+        }
+
+    }
+}
