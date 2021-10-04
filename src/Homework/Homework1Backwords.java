02377a924f796f97cf896944e974f088deaebe3d
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-05 01:37:48 +0400

    Homework1

--- src/Homework/Homework1Backwords.java
+++ src/Homework/Homework1Backwords.java
@@ -3,7 +3,7 @@ package Homework;
 public class Homework1Backwords {
     public static void main(String[] args) {
         for (int i = 1; i < 6; i++) {
-            for (int j = i-2; j < 3  ; j++) {
+            for (int j = i - 2; j < 3; j++) {
                 System.out.print("*");
             }
             System.out.println("*");
