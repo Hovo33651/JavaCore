0424b7288cd92916d48f2cae9676900cb435f0f4
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-05 14:29:48 +0400

    Chapter3Examples

---
+++ src/Chapter3/H.java
@@ -0,0 +1,13 @@
+package Chapter3;
+
+public class H {
+    public static void main(String[] args) {
+        int twoD[][] = new int[4][];
+        twoD[0] = new int[1];
+        twoD[1] = new int[2];
+        twoD[2] = new int[3];
+        twoD[3] = new int[4];
+        int i, j, k = 0;
+        System.out.println();
+    }
+}
