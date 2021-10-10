ffc9d9c394362e93e7ce627da7c23d314f6a6f6d
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-10 12:09:12 +0400

    Homework1

--- src/Homework/FigurePainter.java
+++ src/Homework/FigurePainter.java
@@ -5,7 +5,7 @@ public class FigurePainter {
 
         for (int i = 1; i < 5; i++) {
             for (int j = 0; j < i - 1; j++) {
-                System.out.print("* ");
+                System.out.print("*  ");
             }
             System.out.println("*");
         }
