26ea7417ecd35791a5ec50906ed5e0db9d04034f
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-12 19:49:51 +0400

    NewEvenAndOddValuesArray

--- src/Homework/arrayutil/NewEvenValuesArray.java
+++ src/Homework/arrayutil/NewEvenValuesArray.java
@@ -11,26 +11,22 @@ public class NewEvenValuesArray {
                 System.out.print("array massive values are: ");
             }
             System.out.print(array[i] + " ");
-
         }
         System.out.println();
 
-        int oddNumbers = 0;
+        int oddNumber = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 > 0) {
-                oddNumbers++;
+                oddNumber++;
             }
         }
-
-
-        int[] evenNumbers = new int[array.length - oddNumbers];
+        int[] evenNumbers = new int[array.length - oddNumber];
         int count = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
                 evenNumbers[count] = array[i];
                 count++;
             }
-
         }
 
         for (int i = 0; i < evenNumbers.length; i++) {
@@ -38,10 +34,20 @@ public class NewEvenValuesArray {
                 System.out.print("evenNumber massive values are: ");
             }
             System.out.print(evenNumbers[i] + " ");
+        }
+        System.out.println();
+
+        int[] oddNumbers = new int[oddNumber];
+        for (int i = 0; i < array.length; i++) {
+            if (i==0){
+                System.out.print("oddNumbers massive values are: ");
+            }
+            if(array[i] % 2 > 0){
+                System.out.print(array[i] + " ");;
+            }
 
         }
 
 
     }
-
-}
+    }
