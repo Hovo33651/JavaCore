141485cf0a68ec24176a50a3b6ba717e280f7413
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-12 19:28:51 +0400

    NewEvenValuesArray

--- src/Homework/arrayutil/NewEvenValuesArray.java
+++ src/Homework/arrayutil/NewEvenValuesArray.java
@@ -4,16 +4,26 @@ public class NewEvenValuesArray {
 
     public static void main(String[] args) {
 
-        int[] array = {70, 80, 65,116, 258, 756, 115, 100};
+        int[] array = {70, 80, 65, 116, 258, 756, 115, 100};
 
-        int[] evenNumbers = new int[array.length];
+        int oddNumbers = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 > 0) {
+                oddNumbers++;
+            }
+        }
+
+
+        int[] evenNumbers = new int[array.length - oddNumbers];
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if(array[i] % 2 == 0){
+            if (array[i] % 2 == 0) {
                 evenNumbers[count] = array[i];
                 count++;
             }
+
         }
+
         for (int i = 0; i < evenNumbers.length; i++) {
             System.out.println(evenNumbers[i] + " ");
 
