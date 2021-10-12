331dbcbfc7c10abaa17465eff0b0a4584c58832d
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-12 22:07:08 +0400

    Chapter5Examples

---
+++ src/Chapter5/DoWhile.java
@@ -0,0 +1,12 @@
+package Chapter5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        do{
+            System.out.println("cycle: " + n);
+        }while (--n > 10);
+    }
+}
---
+++ src/Chapter5/While.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+import java.sql.SQLOutput;
+
+public class While {
+    public static void main(String[] args) {
+
+        int n = 10;
+        while(n>0){
+            System.out.println("cycle: " + n);
+            n--;
+        }
+    }
+}
