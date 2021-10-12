e3119d015533d03090669dd8304b4f1784e6e395
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-12 11:54:19 +0400

    ArrayUtil

---
+++ src/Homework/arrayutil/ArrayUtil.java
@@ -0,0 +1,70 @@
+package Homework.arrayutil;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+
+        int[] array = {5, 7, 8, 11, 14, 3, 2, 4, 15, 80};
+
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
+        int maxValue = array[0];
+
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+        System.out.println("massive max value is " + maxValue);
+
+        int minValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+        }
+        System.out.println("massive min value is " + minValue);
+
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                System.out.print("massive even numbers are " + " ");
+            }
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                System.out.print("massive odd numbers are " + " ");
+            }
+            if (array[i] % 2 > 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+
+        int evenCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                evenCount++;
+            }
+        }
+        System.out.println("even numbers count is " + evenCount);
+
+        int oddCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 > 0){
+                oddCount++;
+            }
+        }
+        System.out.println("odd numbers count is " + oddCount);
+}
+}
