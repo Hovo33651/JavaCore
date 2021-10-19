45379a1362e38caf32d209e7c359ca5b440bec1b
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-19 19:54:36 +0400

    ArrayLowToHigh

--- src/Homework/arrayutil/ArrayHelloJava.java
+++ src/Homework/arrayutil/ArrayHelloJava.java
@@ -9,26 +9,26 @@ public class ArrayHelloJava {
         // Գրել կոդ, որը կջնջի միայն սկզբի ու վերջի պռոբելները
         int index = 0;
         int i = 0;
-        char[] barev1 = new char[barev.length];
+        char[] result = new char[barev.length];
         for (i = 0; i < barev.length; i++) {
             if (barev[i] == ' ')
                 continue;
             else if(barev[i] != ' ') {
                 index++;
-                barev1[index] = barev[i];
+                result[index] = barev[i];
                 break;
             }
         }
             for (int j = i; j < barev.length; j++) {
                 if (barev[j] == ' ') {
                     index++;
-                    barev1[index] = barev[j];
+                    result[index] = barev[j];
                 }
                 else if (barev[j] != ' ') {
                     index++;
-                    barev1[index] = barev[j];
+                    result[index] = barev[j];
                 }
-                System.out.print(barev1[index]);
+                System.out.print(result[index]);
             }
         }
     }
