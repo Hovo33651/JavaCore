e79c834afd60e8c1ad80660d9fcf9f68cc71a48b
Hovo33651 <hovogevorgyan94@mail.ru>
2021-10-19 19:11:30 +0400

    ArraySortExample

---
+++ src/Homework/arrayutil/ArraySortExample.java
@@ -0,0 +1,27 @@
+package Homework.arrayutil;
+
+public class ArraySortExample {
+    public static void main(String[] args) {
+
+
+        //Դասավորել մասսիվի թվերը փոքրից մեծ
+        int[] array = {7, 4, 0, 2, 6, 37, 88, 12, 5};
+
+        int tmp = 0;
+        int i = 0;
+        for (i = 0; i < array.length; i++)
+            for (int j = 1; j < array.length; j++)
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + " ");
+        }
+    }
+}
+
+
+
+    
